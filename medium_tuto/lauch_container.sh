3172402dde78243601161b0c58e8f6747525af33
dino <kevin.dinocera@protonmail.com>
2024-08-13 12:09:57 +0200

    add medium tuto

--- medium_tuto/lauch_container.sh
+++ medium_tuto/lauch_container.sh
@@ -3,12 +3,8 @@
 if [ "$1" == "d" ]; then
     docker stop $(docker ps -q)
     docker system prune -af
-    docker build -t name .
-    docker run -itd name
-    while ! docker inspect -f '{{.State.Running}}' name > /dev/null 2>&1; do
-	sleep 1
-    done
-    docker exec -it name /bin/bash
+    docker build -t my_ansible_image . # Construit l'image à partir du Dockerfile dans le répertoire courant
+    docker run --privileged -it my_ansible_image /bin/bash # Exécute le conteneur avec des privilège
 elif [ "$1" == "l" ] && [ -n "$2" ]; then
     docker stop  $(docker ps -q)
     docker system prune -af
